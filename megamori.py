59ccfcb1031be3f973454698d468ef7a9014041a
neomikanagi <74123662+neomikanagi@users.noreply.github.com>
2024-10-16 17:48:55 +0900

    Update megamori.py

--- megamori.py
+++ megamori.py
@@ -6,7 +6,7 @@ url = "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adb
 response = requests.get(url)
 
 rules = {
-    "version": 1,
+    "version": 2,
     "rules": [
         {
             "domain": [],
